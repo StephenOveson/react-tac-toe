7c5a59889afc1dc007013b36d5c3ea516cade226
Spiritbr8ker <stephen.oveson@gmail.com>
2020-02-06 01:26:48 -0700

    Updates

---
+++ precache-manifest.27fdb0d3b03c6042de85d2e9c8138145.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcb0eaa7124a5d72fa386dd7861e829",
+    "url": "/react-tac-toe/index.html"
+  },
+  {
+    "revision": "d03319785dac9fbbb4cd",
+    "url": "/react-tac-toe/static/css/main.2f09bf96.chunk.css"
+  },
+  {
+    "revision": "34d94d5e0254c1c2e3ee",
+    "url": "/react-tac-toe/static/js/2.d0585335.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tac-toe/static/js/2.d0585335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03319785dac9fbbb4cd",
+    "url": "/react-tac-toe/static/js/main.f4baa4d5.chunk.js"
+  },
+  {
+    "revision": "ce61fe1c2f4ba2dd3374",
+    "url": "/react-tac-toe/static/js/runtime-main.0cb3e5fd.js"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/react-tac-toe/static/media/logo.f0793d9f.svg"
+  }
+]);
